e9a3a2e30465a51639395aab7d7cf17a103dd513
ShimFFF <shimseawon1510@gmail.com>
2024-02-17 03:03:00 +0900

    :sparkles: Feat: 페이징, 검색 기능 추가

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -2,7 +2,9 @@ package class101.foo.io;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +25,7 @@ public class PostController {
         );
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2-1. 글 목록을 조회한다. (페이징 x)
     @GetMapping("/posts")
     public List<Post> getPostList() {
         return postRepository.findAll();
@@ -31,13 +33,21 @@ public class PostController {
     
     // 2-2 글 목록을 페이징하여 반환
     @GetMapping("/posts/page")
-    public List<Post> getPostListPaging(@RequestParam Pageable pageable) {
-        Page<Post> posts = postRepository.findAll(pageable);
-        return posts.getContent();
+    public Page<Post> getPostListPaging(@RequestParam(defaultValue = "1") Integer page) { // default page = 1
+        Pageable pageable = PageRequest.of(page - 1, 10, Sort.by("id").descending());
+        return postRepository.findAll(pageable);
     }
     
     // 3. 글 번호로 조회
+    @GetMapping("/posts/{id}}")
+    public Post getPostById(@PathVariable Long id) {
+        return postRepository.findById(id).get();
+    }
     
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
+    @GetMapping("/posts/search")
+    public List<Post> getPostByContent(@RequestParam String content) {
+        return postRepository.findByContentContaining(content);
+    }
 
 }
